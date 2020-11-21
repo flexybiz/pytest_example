c196ac3c0f1579280ec09bfb9a3f7348fa0ec953
flexybiz <flexybiz@gmil.com>
2020-11-21 18:39:26 +0300

    change page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
 
 
 class LoginPage(BasePage):
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -6,5 +7,7 @@ def test_guest_can_go_to_login_page(browser):
     # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
-    # page.go_to_login_page()
+    # page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
